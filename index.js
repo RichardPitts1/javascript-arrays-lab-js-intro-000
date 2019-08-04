add560f9dc0b1e80d7e604e5a8b8e7fad15997a7
Richard Pitts <richardepitts@gmail.com>
2019-08-04 02:26:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
+var kitty = "Numbo";
 
 // Add your functions and code here
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+// destructivelyAppendKitten(kitty)
+// console.log(kittens)
+
+function destructivelyPrependKitten(name) {
+  kittens.
+}
