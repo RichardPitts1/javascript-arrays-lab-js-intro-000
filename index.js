1302cea6444cc7956217010920f0b68d8c918eba
Richard Pitts <richardepitts@gmail.com>
2019-08-04 02:27:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ function destructivelyPrependKitten(name) {
   return kittens
 }
 
-destructivelyPrependKitten(kitty)
-console.log(kittens)
+// destructivelyPrependKitten(kitty)
+// console.log(kittens)
